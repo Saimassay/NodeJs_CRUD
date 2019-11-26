40fbcaae23cb5be66710dd1bd57733e2dc7ebec5
Saimassay <37999471+Saimassay@users.noreply.github.com>
2019-11-26 21:16:12 +0600

    Add files via upload

---
+++ routes/player.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+
+module.exports = {
+    addPlayerPage: (req, res) => {
+        res.render('add-player.ejs', {
+            title: 'Welcome to Socer | Add a new player'
+            ,message: ''
+        });
+    },
+    addPlayer: (req, res) => {
+        if (!req.files) {
+            return res.status(400).send("No files were uploaded.");
+        }
+
+        let message = '';
+        let first_name = req.body.first_name;
+        let last_name = req.body.last_name;
+        let position = req.body.position;
+        let number = req.body.number;
+
+        let usernameQuery = "SELECT * FROM `players` WHERE user_name = '" + username + "'";
+
+        db.query(usernameQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            if (result.length > 0) {
+                message = 'Username already exists';
+                res.render('add-player.ejs', {
+                    message,
+                    title: 'Welcome to Socer | Add a new player'
+                });
+            }
+    });
+    },
+    editPlayerPage: (req, res) => {
+        let playerId = req.params.id;
+        let query = "SELECT * FROM `players` WHERE id = '" + playerId + "' ";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.render('edit-player.ejs', {
+                title: 'Edit  Player'
+                ,player: result[0]
+                ,message: ''
+            });
+        });
+    },
+    editPlayer: (req, res) => {
+        let playerId = req.params.id;
+        let first_name = req.body.first_name;
+        let last_name = req.body.last_name;
+
+        let query = "UPDATE `players` SET `first_name` = '" + first_name + "', `last_name` = '" + last_name + "' WHERE `players`.`id` = '" + playerId + "'";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.redirect('/');
+        });
+    },
+    deletePlayer: (req, res) => {
+        let playerId = req.params.id;
+        let deleteUserQuery = 'DELETE FROM players WHERE id = "' + playerId + '"';
+    }
+};
